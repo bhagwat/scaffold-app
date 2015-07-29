92037d8ab3e0914559e781ccd105570da4a0ccc4
Bhagwat Kumar <bhagwat@intelligrape.com>
2015-07-30 01:48:26 +0530

    refactored tempalte folder name as it was not compatible with Grails 3.x versions

--- sample/grails-app/conf/sample-config.groovy
+++
@@ -1,6 +0,0 @@
-dataSource {
-    username = "root"
-    password = "igdefault"
-    dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-    datbase=jdbc:mysql://localhost/sample_dev
-}
